002cac5382ce7d2a13de1ddcb741bdf73c363772
Jack Weston <jackweston@Jacks-MacBook-Air.local>
2025-02-24 23:27:10 +0000

    Fix: Ensure index.html exists

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,5 @@ import adapter from '@sveltejs/adapter-netlify';
 export default {
   kit: {
     adapter: adapter(),
-    // if your site is served from a subfolder (like /parkers), you can specify it:
-    paths: {
-      base: '/parkers'
-    }
   }
 };
