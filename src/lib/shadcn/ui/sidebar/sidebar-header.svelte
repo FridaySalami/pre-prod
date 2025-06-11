b00f9b9b5dbcb4888b894006e43211bee410938c
Jack Weston <jackweston@gmail.com>
2025-06-11 16:48:03 +0100

    Layout change

---
+++ src/lib/shadcn/ui/sidebar/sidebar-header.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { cn } from "$lib/shadcn/utils/index.js";
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		children,
+		...restProps
+	} = $props();
+</script>
+
+<div
+	bind:this={ref}
+	data-slot="sidebar-header"
+	data-sidebar="header"
+	class={cn("flex flex-col gap-2 p-2", className)}
+	{...restProps}
+>
+	{@render children?.()}
+</div>
---
+++ src/lib/shadcn/ui/sidebar/sidebar-menu.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { cn } from "$lib/shadcn/utils/index.js";
+	let {
+		ref = $bindable(null),
+		class: className,
+		children,
+		...restProps
+	} = $props();
+</script>
+
+<ul
+	bind:this={ref}
+	data-slot="sidebar-menu"
+	data-sidebar="menu"
+	class={cn("flex w-full min-w-0 flex-col gap-1", className)}
+	{...restProps}
+>
+	{@render children?.()}
+</ul>
