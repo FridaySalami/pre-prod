7f20fbe98059fb2af93c619c90b8c4ddb54ecb62
Jack Weston <jackweston@gmail.com>
2025-08-06 16:20:27 +0100

    Build error

--- src/routes/api/rate-limiter-status/+server.ts
+++ src/routes/api/rate-limiter-status/+server.ts
@@ -1,6 +1,6 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-import { amazonFeedsRateLimiter } from '$lib/utils/rate-limiter';
+import { amazonFeedsRateLimiter } from '$lib/utils/rate-limiter.ts';
 
 export const GET: RequestHandler = async () => {
   const status = amazonFeedsRateLimiter.getStatus();
